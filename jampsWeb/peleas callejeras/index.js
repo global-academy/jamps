8b7c8767bbb4e6cf5f806ef70aafff74c29c8bf0
unknown <globalstudent@B205-08.fundacion-jala.local>
2015-08-27 20:21:08 -0400

    first commit

---
+++ jampsWeb/peleas callejeras/index.js
@@ -0,0 +1,24 @@
+$(function(){
+	var nuevocontenido = "",
+		$maincontent =$("#contenido.menu"),
+		$pagewrap  =$("#page-wrap"),
+		tambase   = 0;
+		
+$pageWrap.height.($patgeWarp.height());
+tamBase = $pagewrapWrap.height() - $maincontenido.height();
+
+$(".menu").delegate("a", "click", function(){
+		window.location.hash =$(this).attr("href");
+		return false;
+		});
+
+		$(window).bind('hashchange', function()   {
+
+			nuevo contenido= window.location.hash.substring(1);
+				
+				if(nuevocontenido.length > 0){
+					$
+
+}
+
+});
