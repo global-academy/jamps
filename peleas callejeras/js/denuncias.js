0baa45a6e6fd243aa960d2951031c0edf7cdfcad
Jorge Luis Arteaga Almaraz <jl.arteaga.almaraz@gmail.com>
2015-09-03 00:33:29 -0400

    Changed resource url and adapted complaints display to include pictures

--- peleas callejeras/js/denuncias.js
+++ peleas callejeras/js/denuncias.js
@@ -1,35 +1,7 @@
-function addComplaint(form) {
-    var $form = $(form);
-    var data = {};
-    $form.find('[name]').each(function(idx, input) {
-        var $input = $(input);
-        data[$input.attr('name')] = $input.val();
-    });
-    $.ajax({
-        crossDomain: true,
-        data: JSON.stringify(data),
-        contentType: 'application/json',
-        url: 'https://api.parse.com/1/classes/Complaints',
-        beforeSend: function(xhr) {
-            xhr.setRequestHeader('X-Parse-Application-Id', 'HPjhF9QBvFKLXN4uzPXtZVhyvg21LQXpvduvtZ43');
-            xhr.setRequestHeader('X-Parse-REST-API-Key', '2HIWhWpeg64Ij85pJtF1mOyoe3sUejHTM7KL0NpG')
-        },
-        method: 'POST',
-        success: function(response) {
-            $form.find('[name]').val('');
-            $.each(data, function(prop, value) {
-                response[prop] = value;
-            });
-            addPanel(response);
-        }
-    });
-    return false;
-}
-
 function loadComplaints() {
     $.ajax({
         crossDomain: true,
-        url: 'https://api.parse.com/1/classes/Complaints?order=-createdAt',
+        url: 'https://api.parse.com/1/classes/Report?order=-createdAt',
         beforeSend: function(xhr) {
             xhr.setRequestHeader('X-Parse-Application-Id', 'HPjhF9QBvFKLXN4uzPXtZVhyvg21LQXpvduvtZ43');
             xhr.setRequestHeader('X-Parse-REST-API-Key', '2HIWhWpeg64Ij85pJtF1mOyoe3sUejHTM7KL0NpG')
@@ -47,12 +19,12 @@ function loadComplaints() {
 function addPanel(complaint) {
     var $panel = $('\
         <div class="denuncia"> \
-            <span class="autor">' + complaint.first_name + '</span> \
-            <span class="lugar">' + complaint.location + '</span> \
-            <span class="fechahora">' + formatDate(complaint.createdAt) + '</span> \
-            <p class="descripcion">' + complaint.content + '</p> \
+            <img class="foto" src="'+(complaint.imagen?complaint.imagen.url:'../img/imagen_no_disponible.png')+'" alt="" /> \
+            <b class="descripcion">' + complaint.tipo + '</b> \
+            <span class="lugar">' + complaint.comentario + '</span> \
+            <small><span class="fechahora">' + formatDate(complaint.createdAt) + '</span></small> \
         </div>');
-    $panel.hide().insertAfter('#formulario').fadeIn({
+    $panel.hide().appendTo('.contenedor').fadeIn({
         duration: 'slow',
         queue: true
     });
